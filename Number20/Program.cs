5a98913c91d294bd6d9cba5e4c76803372f27818
Dylariz <zarec-k@mail.ru>
2023-11-09 18:17:26 +0300

    Complete simple part of task

--- Number20/Program.cs
+++ Number20/Program.cs
@@ -1,40 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Number20
+namespace Number20;
+
+internal static class Program
 {
-    internal static class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        int simulationYear = 2020;
+        List<Student> students = new List<Student>();
+        List<Student> result = new List<Student>();
+            
+        // Симуляция 5 лет
+        for (int i = 0; i < 5; i++)
         {
-            int simulationYear = 2020;
-            List<Student> students = new List<Student>();
-            
-            // Симуляция 5 лет
-            for (int i = 0; i < 5; i++)
+            Console.Write($"Введите количество поступивших студентов в {simulationYear}-м году: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Перевод студентов на следующий курс
+            for (var j = students.Count - 1; j >= 0; j--)
             {
-                Console.Write($"Введите количество поступивших студентов в {simulationYear}-м году: ");
-                int n = int.Parse(Console.ReadLine());
-
-                // Перевод студентов на следующий курс
-                foreach (var t in students)
+                if (students[j].NextCourse() == -1)
                 {
-                    if (t.NextCourse() == -1)
-                    {
-                        students.Remove(t);
-                    }
+                    students.RemoveAt(j);
                 }
-                
-                // Добавление новых студентов в список
-                for (int j = 0; j < n; j++)
-                {
-                    students.Add(new Student(simulationYear));
-                }
-
-                simulationYear++;
-                Console.Clear();
             }
-            
+
+            // Добавление новых студентов в список
+            for (int j = 0; j < n; j++)
+            {
+                students.Add(new Student(simulationYear));
+            }
+
+            simulationYear++;
+            Console.Clear();
+        }
+
+        List<(string, int)> bistStudents = new List<(string, int)>();
+        foreach (var t in students)
+        {
+            if (t.Group.Contains("БИСТ"))
+            {
+                bistStudents.Add((t.Name, t.Course));
+            }
+        }
+        
+        foreach (var t in students)
+        {
+            if (t.Group.Contains("БИВТ") && bistStudents.Contains((t.Name, t.Course)))
+            {
+                result.Add(t);
+            }
+        }
+
+        string sep = new string('—', 40);
+
+        Console.WriteLine(sep);
+        foreach (var t in result)
+        {
+            Console.WriteLine($"{t}\n{sep}");
         }
     }
 }
--- Number20/Student.cs
+++ Number20/Student.cs
@@ -1,100 +1,146 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
-namespace Number20
+namespace Number20;
+
+public class Student
 {
-    public class Student
+    public enum Genders
     {
-        public enum Genders
+        Male,
+        Female
+    }
+
+    private static Random _random = new();
+
+    private static readonly string[] MaleNames =
+    {
+        "Александр", "Алексей", "Анатолий", "Андрей", "Антон", "Аркадий", "Борис", "Вадим", "Валентин", "Валерий",
+        "Василий", "Виктор", "Виталий", "Владимир", "Владислав", "Геннадий", "Георгий", "Григорий", "Даниил",
+        "Денис", "Дмитрий", "Евгений", "Егор", "Иван", "Игорь", "Кирилл", "Константин", "Лев", "Леонид", "Максим",
+        "Михаил", "Никита", "Николай", "Олег", "Павел", "Роман", "Сергей", "Станислав", "Тимофей", "Юрий", "Ярослав"
+    };
+
+    private static readonly string[] MaleSurnames =
+    {
+        "Иванов", "Петров", "Сидоров", "Кузнецов", "Смирнов", "Михайлов", "Федоров", "Соколов", "Яковлев", "Попов",
+        "Андреев", "Алексеев", "Александров", "Лебедев", "Григорьев", "Степанов", "Николаев", "Орлов", "Козлов",
+        "Васильев", "Зайцев", "Павлов", "Семенов", "Голубев", "Виноградов", "Богданов", "Воробьев", "Фролов",
+        "Матвеев", "Дмитриев", "Жуков", "Белов", "Комаров", "Осипов", "Киселев", "Тихонов", "Макаров", "Анисимов",
+        "Ершов", "Родионов", "Коновалов", "Лазарев", "Абобит"
+    };
+
+    private static readonly string[] MalePatronymics =
+    {
+        "Александрович", "Алексеевич", "Анатольевич", "Андреевич", "Антонович", "Богданович", "Борисович",
+        "Вадимович", "Валентинович", "Валерьевич", "Васильевич", "Викторович", "Витальевич", "Владимирович",
+        "Владиславович", "Вячеславович", "Геннадьевич", "Георгиевич", "Григорьевич", "Данилович", "Денисович",
+        "Дмитриевич", "Евгеньевич", "Егорович", "Иванович", "Игоревич", "Кириллович", "Константинович",
+        "Леонидович", "Максимович", "Матвеевич", "Михайлович", "Никитич", "Николаевич", "Олегович", "Павлович",
+        "Петрович", "Романович", "Сергеевич", "Станиславович", "Юрьевич"
+    };
+
+    private static readonly string[] FemaleNames =
+    {
+        "Александра", "Алина", "Алла", "Анастасия", "Ангелина", "Анна", "Антонина", "Валентина", "Валерия",
+        "Варвара", "Василиса", "Вера", "Вероника", "Виктория", "Галина", "Дарья", "Евгения", "Екатерина", "Елена",
+        "Жанна", "Зинаида", "Зоя", "Инга", "Инесса", "Ирина", "Карина", "Кира", "Клавдия", "Кристина", "Лариса",
+        "Лидия", "Лилия", "Любовь", "Маргарита", "Марина", "Мария", "Надежда", "Наталья", "Оксана", "Ольга",
+        "Светлана", "Татьяна"
+    };
+
+    private static readonly string[] FemaleSurnames =
+    {
+        "Иванова", "Петрова", "Сидорова", "Кузнецова", "Смирнова", "Козлова", "Новикова", "Морозова", "Волкова",
+        "Зайцева", "Павлова", "Соловьева", "Васильева", "Попова", "Андреева", "Козлова", "Николаева", "Орлова",
+        "Киселева", "Макарова", "Антонова", "Григорьева", "Лебедева", "Соколова", "Князева", "Белова", "Федорова",
+        "Тихонова", "Комарова", "Карпова", "Беляева", "Щербакова", "Гущина", "Герасимова", "Медведева", "Дмитриева",
+        "Калинина", "Алексеева", "Степанова", "Яковлева", "Сорокина"
+    };
+
+    private static readonly string[] FemalePatronymics =
+    {
+        "Александровна", "Алексеевна", "Анатольевна", "Андреевна", "Антоновна", "Аркадьевна", "Арсеньевна",
+        "Богдановна", "Борисовна", "Вадимовна", "Валентиновна", "Валерьевна", "Васильевна", "Викторовна",
+        "Витальевна", "Владимировна", "Владиславовна", "Вячеславовна", "Геннадьевна", "Георгиевна", "Григорьевна",
+        "Даниловна", "Денисовна", "Дмитриевна", "Евгеньевна", "Егоровна", "Ивановна", "Игоревна", "Ильинична",
+        "Кирилловна", "Константиновна", "Леонидовна", "Максимовна", "Матвеевна", "Михайловна", "Николаевна",
+        "Олеговна", "Павловна", "Петровна", "Романовна", "Сергеевна"
+    };
+
+    public string Surname { get; private set; }
+    public string Name { get; private set; }
+    public string Patronymic { get; private set; }
+    public Genders Gender { get; private set; }
+    public DateTime DateOfBirth { get; private set; }
+    public string Group { get; private set; } // БИСТ-хх, БИВТ-xх, БПИ-xх
+    public Dictionary<string, int> Marks { get; private set; }
+    public int Course { get; private set; }
+
+    public Student(int simulationYear)
+    {
+        Gender = (Genders)_random.Next(2);
+
+        if (Gender == Genders.Male)
         {
-            Male,
-            Female
+            Surname = MaleSurnames[_random.Next(MaleSurnames.Length)];
+            Name = MaleNames[_random.Next(MaleNames.Length)];
+            Patronymic = MalePatronymics[_random.Next(MalePatronymics.Length)];
         }
-        
-        private static Random _random = new Random();
-
-        private static readonly string[] Names =
+        else
         {
-            "Александр", "Алексей", "Анатолий", "Андрей", "Антон", "Аркадий", "Борис", "Вадим", "Валентин", "Валерий",
-            "Василий", "Виктор", "Виталий", "Владимир", "Владислав", "Геннадий", "Георгий", "Григорий", "Даниил",
-            "Денис", "Дмитрий", "Евгений", "Егор", "Иван", "Игорь", "Кирилл", "Константин", "Лев", "Леонид", "Максим",
-            "Михаил", "Никита", "Николай", "Олег", "Павел", "Роман", "Сергей", "Станислав", "Тимофей", "Юрий", "Ярослав"
-        };
-
-        private static readonly string[] Surnames =
-        {
-            "Иванов", "Петров", "Сидоров", "Кузнецов", "Смирнов", "Михайлов", "Федоров", "Соколов", "Яковлев", "Попов",
-            "Андреев", "Алексеев", "Александров", "Лебедев", "Григорьев", "Степанов", "Николаев", "Орлов", "Козлов",
-            "Васильев", "Зайцев", "Павлов", "Семенов", "Голубев", "Виноградов", "Богданов", "Воробьев", "Фролов",
-            "Матвеев", "Дмитриев", "Жуков", "Белов", "Комаров", "Осипов", "Киселев", "Тихонов", "Макаров", "Анисимов",
-            "Ершов", "Родионов", "Коновалов", "Лазарев"
-        };
-
-        private static readonly string[] Patronymics =
-        {
-            "Александрович", "Алексеевич", "Анатольевич", "Андреевич", "Антонович", "Богданович", "Борисович",
-            "Вадимович", "Валентинович", "Валерьевич", "Васильевич", "Викторович", "Витальевич", "Владимирович",
-            "Владиславович", "Вячеславович", "Геннадьевич", "Георгиевич", "Григорьевич", "Данилович", "Денисович",
-            "Дмитриевич", "Евгеньевич", "Егорович", "Иванович", "Игоревич", "Кириллович", "Константинович",
-            "Леонидович", "Максимович", "Матвеевич", "Михайлович", "Никитич", "Николаевич", "Олегович", "Павлович",
-            "Петрович", "Романович", "Сергеевич", "Станиславович", "Юрьевич"
-        };
-
-        public string Surname { get; private set; }
-        public string Name { get; private set; }
-        public string Patronymic { get; private set; }
-        public Genders Gender { get; private set; } // True - Мужчина, False - Женщина
-        public DateTime DateOfBirth { get; private set; }
-        public string Group { get; private set; } // БИСТ-хх, БИВТ-xх, БПИ-xх
-        public Dictionary<string, int> Marks { get; private set; }
-        public int Course { get; private set; }
-
-        public Student(int simulationYear)
-        {
-            Surname = Surnames[_random.Next(Surnames.Length)];
-            Name = Names[_random.Next(Names.Length)];
-            Patronymic = Patronymics[_random.Next(Patronymics.Length)];
+            Surname = FemaleSurnames[_random.Next(FemaleSurnames.Length)];
+            Name = FemaleNames[_random.Next(FemaleNames.Length)];
+            Patronymic = FemalePatronymics[_random.Next(FemalePatronymics.Length)];
+        }
             
-            Gender = (Genders)_random.Next(2);
-            
-            DateOfBirth = new DateTime(_random.Next(simulationYear - 22, simulationYear - 16), _random.Next(1, 13),
-                _random.Next(1, 29));
 
-            var imulationYearStr = simulationYear.ToString().Substring(2);
-            switch (_random.Next(3))
-            {
-                case 0:
-                    Group = "БИСТ-" + imulationYearStr;
-                    break;
-                case 1:
-                    Group = "БИВТ-" + imulationYearStr;
-                    break;
-                case 2:
-                    Group = "БПИ-" + imulationYearStr;
-                    break;
-            }
+        DateOfBirth = new DateTime(_random.Next(simulationYear - 22, simulationYear - 16), _random.Next(1, 13),
+            _random.Next(1, 29));
 
-            Course = 1;
-            Marks = new Dictionary<string, int>
-            {
-                { "Математика", _random.Next(45, 101) },
-                { "Информатика", _random.Next(45, 101) },
-                { "Русский язык", _random.Next(45, 101) }
-            };
-        }
-
-        public int NextCourse()
+        var simulationYearStr = simulationYear.ToString().Substring(2);
+        switch (_random.Next(3))
         {
-            if (Course < 4)
-            {
-                Course++;
-            }
-            else
-            {
-                return -1;
-            }
-
-            return Course;
+            case 0:
+                Group = "БИСТ-" + simulationYearStr;
+                break;
+            case 1:
+                Group = "БИВТ-" + simulationYearStr;
+                break;
+            case 2:
+                Group = "БПИ-" + simulationYearStr;
+                break;
         }
+
+        Course = 1;
+        Marks = new Dictionary<string, int>
+        {
+            { "Математика", _random.Next(45, 101) },
+            { "Информатика", _random.Next(45, 101) },
+            { "Русский язык", _random.Next(45, 101) }
+        };
+    }
+
+    public int NextCourse()
+    {
+        if (Course < 4)
+        {
+            Course++;
+        }
+        else
+        {
+            return -1;
+        }
+
+        return Course;
+    }
+
+    public override string ToString()
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append($"ФИО: {Surname} {Name} {Patronymic}\n");
+        stringBuilder.Append($"Группа: {Group}");
+        return stringBuilder.ToString();
     }
 }
